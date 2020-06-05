feda4afd055374abfb5c7e95ac03472f214dfe99
Vincent Chiang <vincentchiang@berkeley.edu>
2020-06-04 23:02:56 -0700

    finished setup

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -1,3 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
+import time
 
 app = Flask("__main__")
+
+
+@app.route("/")
+def my_index():
+    return render_template("index.html", token="Hello from Flask")
+
+
+@app.route("/time")
+def get_time():
+    return jsonify({"time": time.time()})
+
+
+app.run(debug=True)
---
+++ flask-backend/static/react/precache-manifest.db0eb3d8b86da6ab6ba1399ade686e4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c3bc332e3153130bd102ae22ecb807a",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "5750f338db6e8e158bfe",
+    "url": "/static/react/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3277d6798c50ee1413bd",
+    "url": "/static/react/js/2.441c055e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/react/js/2.441c055e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5750f338db6e8e158bfe",
+    "url": "/static/react/js/main.37f23489.chunk.js"
+  },
+  {
+    "revision": "c7241993fd2c7bed7213",
+    "url": "/static/react/js/runtime-main.b9f37f5d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/media/logo.5d5d9eef.svg"
+  }
+]);
